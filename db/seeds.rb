86819bd72c3202df914ab7ea0a6ccc5a25c17a67
Raquel Tessarollo <tessarollo.raquel@gmail.com>
2020-08-18 16:39:16 +0200

    FInished a Rails Wikinimous page with a list pf articles generated with Faker, able to be deleted and edit and read one by one

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require 'faker'
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words,
+  )
+  article.save!
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
